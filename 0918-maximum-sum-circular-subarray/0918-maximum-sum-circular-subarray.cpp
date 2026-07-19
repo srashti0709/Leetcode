5bfbd352669818bd9a74b6996e82b287e852b84e
Srashti Shakya <srashtishakya07@gmail.com>
2026-07-19 19:25:54 +0530

    Time: 3 ms (19.21%), Space: 43.5 MB (78.77%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int kadanesMax(vector<int>& nums,int n){
+        int sum = nums[0];
+        int maxSum = nums[0];
+        for(int i=1;i<n;i++){
+            sum = max(sum+nums[i],nums[i]);
+            maxSum = max(sum,maxSum);
+        }
+        return maxSum;
+    }
+    int kadanesMin(vector<int>& nums,int n){
+        int sum = nums[0];
+        int minSum = nums[0];
+        for(int i=1;i<n;i++){
+            sum = min(sum+nums[i],nums[i]);
+            minSum = min(sum,minSum);
+        }
+        return minSum;
+    }
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        int SUM = accumulate(nums.begin(),nums.end(),0);
+        int minSum = kadanesMin(nums,n);
+        int maxSum = kadanesMax(nums,n);
+        int circular_Sum = SUM - minSum;
+        if(maxSum > 0){
+            return max(maxSum,circular_Sum);
+        }
+        return maxSum;
+    }
+};
